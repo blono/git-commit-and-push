22fa01546d7ab52bc786c8c9ff1dcd01f2e6f70f
author <author@example.com>
2021-11-23 13:12:42 +0900

    slight modification

---
+++ README.md
@@ -0,0 +1,5 @@
+# Git Commit & Push(Sync)
+
+Execute git commit & push(sync) from the icon or menu item.
+
+![Sample](./images/readme/sample.png)
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('git-commit-and-push.commitAndPush', () => {
-		return vscode.commands.executeCommand("git.commitStaged").then(() => {
-			return vscode.commands.executeCommand("git.sync").then(() => {
-				vscode.window.showInformationMessage('Committed and pushed successfully');
-			});
-		});
-	});
+    const disposable = vscode.commands.registerCommand('git-commit-and-push.commitAndPush', () => {
+        return vscode.commands.executeCommand("git.commitStaged").then(() => {
+            return vscode.commands.executeCommand("git.sync").then(() => {
+                vscode.window.showInformationMessage('Committed and pushed successfully');
+            });
+        });
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
