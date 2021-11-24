052ed400cf224c86edd0a93a973dc3c21643ec92
author <author@example.com>
2021-11-24 16:20:01 +0900

    support ja

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
+import * as nls from 'vscode-nls';
+
+const localize = nls.config({ messageFormat: nls.MessageFormat.file })();
+
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('git-commit-and-push.commitAndPush', () => {
         return vscode.commands.executeCommand("git.commitStaged").then(() => {
             return vscode.commands.executeCommand("git.sync").then(() => {
-                vscode.window.showInformationMessage('Committed and pushed successfully');
+				const message = localize('commitAndPush.text', 'Committed and pushed successfully');
+                vscode.window.showInformationMessage(message);
             });
         });
     });
@@ -12,5 +17,4 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
